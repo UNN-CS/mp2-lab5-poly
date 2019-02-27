e435b9dcd9ed26d81755c489329bcb408b3aedc7
Zhivaev Artem <zhivaev993@gmail.com>
2019-02-27 15:15:53 +0300

    headring.hpp

---
+++ include/headring.h
@@ -0,0 +1,12 @@
+#ifndef __HEADRING_H__
+#define __HEADRING_H__
+
+#include "datlist.h"
+
+class THeadRing: public TDatList
+{
+
+};
+
+#endif
+
---
+++ src/headring.cpp
@@ -0,0 +1,23 @@
+#include "../include/headring.h"
+
+THeadRing::THeadRing()
+{
+
+}
+
+THeadRing::~THeadRing()
+{
+
+}
+
+void THeadRing::InsFirst(PTDatValue pVal)
+{
+
+}
+
+void THeadRing::DelFirst()
+{
+
+}
+
+
