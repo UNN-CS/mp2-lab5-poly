0a0bb25f9c140e71ddb46f1f6450415a4f29ffd3
ksc <ksc@S.local>
2019-04-23 15:09:50 +0300

    Work, u God damn mug

---
+++ include/headring.h
@@ -0,0 +1,13 @@
+#include "datlist.h"
+
+class THeadRing : public TDatList {
+protected:
+	PTDatLink pHead;     
+public:
+	THeadRing();
+	~THeadRing();
+    
+	virtual void InsFirst(PTDatValue pVal = NULL); 
+												   
+	virtual void DelFirst(void);                 
+};
