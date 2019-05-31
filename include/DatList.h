1282b6569d4076f17e0b7531f325db774457e601
Татьяна Кондрина <kondrina.tanya@gmail.com>
2019-05-31 20:59:09 +0300

    Add files

---
+++ include/DatList.h
@@ -0,0 +1,41 @@
+#ifndef __DATLIST_H__
+#define __DATLIST_H__
+
+#include "DatLink.h"
+
+class TDatList {
+protected:
+    PTDatLink pFirst;   
+    PTDatLink pLast;    
+    PTDatLink pCurrLink;
+    PTDatLink pPrevLink;
+    PTDatLink pStop;   
+    int CurrPos;
+    int ListLen;
+protected:  
+    PTDatLink GetLink(PTDatValue pVal = NULL, PTDatLink pLink = NULL);
+    void      DelLink(PTDatLink pLink);   
+public:
+    TDatList();
+    ~TDatList() { DelList(); }
+    
+    PTDatValue GetDatValue(TLinkPos mode = CURRENT) const; 
+    virtual int IsEmpty()  const { return pFirst == pStop; } 
+    int GetListLength()    const { return ListLen; }       
+
+    int SetCurrentPos(int pos);          
+    int GetCurrentPos(void) const;       
+    virtual int Reset(void);             
+    virtual int IsListEnded(void) const; 
+    int GoNext(void);                    
+
+    virtual void InsFirst(PTDatValue pVal = NULL); 
+    virtual void InsLast(PTDatValue pVal = NULL); 
+    virtual void InsCurrent(PTDatValue pVal = NULL); 
+                                   
+    virtual void DelFirst(void);   
+    virtual void DelCurrent(void); 
+    virtual void DelList(void);    
+};
+
+#endif 
