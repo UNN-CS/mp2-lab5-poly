4a3208e7a1aee03d67520f2a641df10fa787a160
unknown <tappanoukimi18@gmail.com>
2019-06-04 13:07:11 +0300

    b43

---
+++ src/HeadRing.cpp
@@ -0,0 +1,32 @@
+#include "HeadRing.h"
+
+THeadRing::THeadRing()
+{
+	InsLast();
+	pHead = pFirst;
+	list_len = 0;
+	pStop = pHead;
+	Reset();
+	pFirst->SetNextLink(pFirst);
+}
+/*****************************************************************************/
+
+THeadRing::~THeadRing()
+{
+	DelLink(pHead);
+	pHead = NULL;
+}
+/*****************************************************************************/
+
+void THeadRing::InsFirst(PTDatValue pVal)
+{
+	TDatList::InsFirst(pVal);
+	pHead->SetNextLink(pFirst);
+}
+/*****************************************************************************/
+
+void THeadRing::DelFirst()
+{
+	TDatList::DelFirst();
+	pHead->SetNextLink(pFirst);
+}
